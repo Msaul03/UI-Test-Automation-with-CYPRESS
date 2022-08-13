d4e8dad01804e6cb34f3a77dda382ca4129c21ee
Saul Martinez <saulalejandro41.0308@gmail.com>
2022-08-12 19:13:19 -0500

    Se añade el proyecto

---
+++ cypress/e2e/2-advanced-examples/primerPrueba.cy.js
@@ -0,0 +1,38 @@
+describe('Primer Suite de Pruebas', () => {
+
+    describe('Primer Suite de Pruebas', () => {
+        it('primer prueba', () => {
+            cy.visit('https://youtube.com');
+        });
+        it('segunda prueba', () => {
+            cy.visit('https://youtube.com');
+        });
+        it('tercera prueba', () => {
+            cy.visit('https://youtube.com');
+        });
+
+    });
+    it('primer prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+    it('segunda prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+    it('tercera prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+
+});
+
+describe('Segunda Suite de Pruebas', () => {
+    it('primer prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+    it('segunda prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+    it('tercera prueba', () => {
+        cy.visit('https://youtube.com');
+    });
+
+});
